d9c8551b8c1e0b23f6f5f4549bb7116e932b5859
unknown <chenwang@acrabsoft.com>
2020-11-17 14:11:34 +0800

    For the second question

--- Solution.java
+++ Solution.java
@@ -3,6 +3,7 @@ package com.acrabsoft.web;
 import org.junit.Test;
 
 public class Solution {
+    // For the first question
     @Test
     public void testBird() {
         Animal animal = new Bird();
@@ -11,6 +12,38 @@ public class Solution {
         Bird bird = (Bird) animal;
         bird.fly();
         bird.sing();
+    }
+
+    // For the second question
+    @Test
+    public void testOtherBird() {
+        // duck
+        Animal animal = new Duck();
+        animal.walk();
+
+        Bird bird = (Bird) animal;
+        bird.fly();
+        bird.sing();
+
+        Duck duck = (Duck)bird;
+        duck.say();
+        duck.swim();
+        checkfly(duck);
+        // chicken
+        Animal animal2 = new Chicken();
+        Chicken chicken = (Chicken)animal2;
+        chicken.say();
+        checkfly(chicken);
+    }
+
+    public void checkfly(Animal animal) {
+        if(animal instanceof Chicken) {
+            Chicken chicken = (Chicken)animal;
+            chicken.fly();
+        }else if(animal instanceof Duck) {
+            Duck duck = (Duck)animal;
+            duck.fly();
+        }
     }
 }
 
@@ -28,7 +61,24 @@ class Bird extends Animal {
     void sing() {
         System.out.println("I am singing");
     }
-
 }
 
+class Duck extends Bird{
+    void say() {
+        System.out.println("I can say Quack, quack!");
+    }
+    void swim() {
+        System.out.println("I can swim!");
+    }
+}
+
+class Chicken extends Bird{
+    void say() {
+        System.out.println("I can say Cluck, cluck!");
+    }
+
+    void fly() {
+        System.out.println("I am a chicken and i can not fly!");
+    }
+}
 
